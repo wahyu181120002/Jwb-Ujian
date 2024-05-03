408bc975b8455e333eb08e5a5574e9bc4b27fc29
wahyu18112002 <muhammadwahyuhidayat18@gmail.com>
2024-05-03 11:36:50 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,19 @@
+<p></p>
+<body>
+    <center><font size="+3" face="arial">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="+3" face="arial">UNIVERSITAS HARAPAN MEDAN</font></center><br>
+
+
+
+    
+		<div id="header">
+			<div id="Mantap-container">
+            <center><image id="pasfoto" src="pasfoto.jpg" alt="copyright 2024-Muhammad Wahyu Hidayat"  width="300" height="300" border=0 style="border:0; text-decoration:none; outline:none"> </center>
+    
+			</div>
+
+
+    <center><p>copyright 2024-MUHAMMAD WAHYU HIDAYAT</p></center>
+    
+
+</body>
